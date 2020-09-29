0b35e9c84ec6446eab0b6e5e51e0103d527eebde
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-09-30 00:07:23 +0200

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) !== true || array.length === 0) return 0;
+    let min = 0;
+    array.filter(item => min > item ? min = item : min);
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) !== true || array.length === 0) return 0;
+    let max = 0;
+    array.filter(item => max < item ? max = item : max);
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) !== true || array.length === 0) return 0;
+    return array.reduce((sum, item) => sum += item, 0) / array.length;
 }
