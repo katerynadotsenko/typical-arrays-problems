5dd1e9d202f69344a45d94b8f28b9be4f7ccc6fa
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-10-02 11:02:33 +0200

    fix: help - main page section

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 exports.min = function min(array) {
     if (Array.isArray(array) !== true || array.length === 0) return 0;
     let min = 0;
-    array.filter(item => min > item ? min = item : min);
+    array.forEach(item => min > item ? min = item : min);
     return min;
 }
 
 exports.max = function max(array) {
     if (Array.isArray(array) !== true || array.length === 0) return 0;
     let max = 0;
-    array.filter(item => max < item ? max = item : max);
+    array.forEach(item => max < item ? max = item : max);
     return max;
 }
 
